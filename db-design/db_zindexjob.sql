6808f8fc6bf36e8c7134a340b2ae979fdad9d345
dukai <dukai86@gmail.com>
2012-09-20 18:07:46 +0800

    commit database design

---
+++ db-design/db_zindexjob.sql
@@ -0,0 +1,126 @@
+-- phpMyAdmin SQL Dump
+-- version 3.2.4
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Sep 20, 2012 at 10:06 AM
+-- Server version: 5.1.41
+-- PHP Version: 5.3.1
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `db_zindexjob`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `companies`
+--
+
+CREATE TABLE IF NOT EXISTS `companies` (
+  `company_id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` text NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `map_url` varchar(255) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`company_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `compay_contactuser_rel`
+--
+
+CREATE TABLE IF NOT EXISTS `compay_contactuser_rel` (
+  `company_id` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `compay_job_rel`
+--
+
+CREATE TABLE IF NOT EXISTS `compay_job_rel` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `compay_id` int(11) NOT NULL,
+  `job_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact_users`
+--
+
+CREATE TABLE IF NOT EXISTS `contact_users` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `qq` varchar(100) NOT NULL,
+  `gtalk` varchar(100) NOT NULL,
+  `msn` varchar(100) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `cellphone` varchar(20) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  PRIMARY KEY (`uid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobs`
+--
+
+CREATE TABLE IF NOT EXISTS `jobs` (
+  `job_id` int(11) NOT NULL,
+  `title` int(11) NOT NULL,
+  `address` int(11) NOT NULL,
+  `pay` int(11) NOT NULL COMMENT '工资',
+  `treatment` int(11) NOT NULL COMMENT '待遇',
+  `duty` int(11) NOT NULL COMMENT '职责',
+  `requirement` int(11) NOT NULL COMMENT '要求',
+  `person_number` varchar(100) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job_categories`
+--
+
+CREATE TABLE IF NOT EXISTS `job_categories` (
+  `jc_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `description` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job_category_rel`
+--
+
+CREATE TABLE IF NOT EXISTS `job_category_rel` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `job_id` int(11) NOT NULL,
+  `jc_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
