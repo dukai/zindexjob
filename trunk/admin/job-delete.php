277c05cd9eba3995f60623a119badfd9611a96f8
DK <dukai86@gmail.com>
2012-09-26 23:22:59 +0800

    modify job delete

--- trunk/admin/job-delete.php
+++ trunk/admin/job-delete.php
@@ -5,7 +5,7 @@ $jobId = $_GET['job_id'];
 if(empty($jobId)){
 	echo "unknow job id";
 }else{
-	$jcId = intval($jobId);
+	$jobId = intval($jobId);
 	$db->query("delete from jobs where job_id={$jobId}");
 	header('Content-Type: text/html; charset=utf-8');
 	header('refresh:3;url=job-category-manage.php');
