127016ffe2e0396baae054dc1dd134ab2ae88785
DK <dukai86@gmail.com>
2012-11-29 23:06:27 +0800

    fix industry and nature selector helper error

--- trunk2/module/Admin/src/Admin/Helper/CompanyNatureSelector.php
+++ trunk2/module/Admin/src/Admin/Helper/CompanyNatureSelector.php
@@ -5,7 +5,7 @@ namespace Admin\Helper;
 use Zend\View\Helper\AbstractHelper;
 
 class CompanyNatureSelector extends AbstractHelper{
-	public function __invoke($values, $selected = 0, $name = 'scale'){
+	public function __invoke($values, $selected = 0, $name = 'nature'){
 		$html = "<select id=\"{$name}\" name=\"{$name}\">";
 		foreach($values as $key=>$value){
 			if($selected == $value['id']){
